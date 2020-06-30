55949ed6bbd662bffce04b99c8887b21a5b8677f
Sudarshan Mandal <sudarshananjanamandal@gmail.com>
2020-06-30 13:46:26 +0530

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,4 @@
+#a=int(input("Enter the number: "))
+#num_sqrt=a**0.5
+#print("the square root of %0.3f is %0.3f"%(a,num_sqrt))
+print("the square root is %0.3f"%float(input("Enter the Number: ")*float(0.5)))
