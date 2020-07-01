24dc6541a33adbddf9a28ba7e285c6afe90a4e9b
Sudarshan Mandal <sudarshananjanamandal@gmail.com>
2020-07-01 11:32:24 +0530

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,4 @@
+a=float(input("Enter the number: "))
+num_sqrt=a**0.5
+print("the square root of %0.f is %0.f"%(a,num_sqrt))
+#print("the square root is %0.3f"%float(input("Enter the Number: ")*float(0.5)))
